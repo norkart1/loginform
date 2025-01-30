efd965d8315d25388054d99fffd4a7bcf193f836
muhammednavasms <muhammednavasms@gmail.com>
2025-01-30 12:01:59 +0000

    Update

--- config/database.js
+++ config/database.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://axo:axoop@cluster0.imckmpc.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-        useUnifiedTopology: true
-        })
-        .then(() => console.log('MongoDB Connected'))
-        .catch(err => console.log(err));
-        
+mongoose.connect('mongodb+srv://axo:axoop@cluster0.imckmpc.mongodb.net/?retryWrites=true&w=majority')
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
