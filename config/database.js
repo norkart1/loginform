b87ed4e9514c7b4d599b200aa0f3bfaae595fdd4
Capt Nork <138743746+norkart1@users.noreply.github.com>
2025-03-21 15:38:40 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://axo:axoop@cluster0.imckmpc.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('')
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
